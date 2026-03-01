4b9c2a2124dce823ed737ed5d16158520c4cae92
reagandexteroltiveros-star <reagandexter.oltiveros@gmail.com>
2026-03-02 00:03:26 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Telegram Bot configuration
+define('TELEGRAM_BOT_TOKEN', '8414483455:AAGs6rmmLdkx-uFCkpx3-9AEpFXEDXxEeXI');
+define('TELEGRAM_CHAT_ID', '5863793961');
+
+// OMADA device info
+define('OMADA_WIFI_NAME', 'GELAI WIFI VOUCHER');
+define('OMADA_SITE', 'Sari Sari Store');
+
+// Log file path
+define('LOG_FILE', __DIR__ . '/log/events.log');
+?>
